c6d5de8c9a7fd6e45abaa9d9f9d53e6f7847a3ca
Anton Konopelkin <anti.kon.40@yandex.ru>
2024-04-09 20:02:34 +0300

    Fix

--- MetodOptLab4/main.py
+++ MetodOptLab4/main.py
@@ -1,141 +1,53 @@
-import random
-import matplotlib.pyplot as plt
+import time
+import eel
 import numpy as np
+import matplotlib.pyplot as plt
 
-f = "((x - 1) ** 2) / (1) + ((y + 3) ** 2) / (0.4)"
+@eel.expose
+def get_fibonacci_method_result(function, a, b, delta):
+    # answer = []
+    # for i in [0.1, 0.01, 0.001]:
+    #     print(logic.fibonacciMethod(a, b, i, delta, logic.string_to_function(function)))
+    #     x_p, R, d, S = logic.fibonacciMethod(a, b, i, delta, logic.string_to_function(function))
+    #     answer.append([i, x_p, R, d, S])
+    #     x = np.arange(a, b, 0.01)
+    #     plt.clf()
+    #     plt.xlabel("x")
+    #     plt.ylabel("y")
+    #     y = [logic.string_to_function(function)(element) for element in x]
+    #     plt.plot(x, y)
+    #     plt.errorbar(x_p, R, xerr= d, marker='o', linestyle='none',
+    #     ecolor='k', elinewidth=0.8, capsize=4, capthick=1)
+    #     plt.grid()
+    #     name = "image/image" + str(i) + "_" + str(time.time()) + ".png"
+    #     plt.savefig("web/" + name, transparent=True)
+    #     answer[-1].append(name)
+    #     print(answer)
+    return 0
 
-def string_to_function(expression):
-    def function(x, y):
-        return eval(expression)
-    return function
-
-
-def derivative_x(x, y, increment, function):
-    return (function(x + increment, y) - function(x, y)) / increment
-
-
-def derivative_y(x, y, increment, function):
-    return (function(x, y + increment) - function(x, y)) / increment
-
-
-def gradient(x, y, epsilon, function):
-    return [[derivative_x(x, y, epsilon, function)], [derivative_y(x, y, epsilon, function)]]
-
-
-def derivative_xx(x, y, increment, function):
-    return (function(x + increment, y) - 2 * function(x, y) + function(x - increment, y)) / (increment ** 2)
-
-
-def derivative_xy(x, y, increment, function):
-    return (derivative_y(x + increment, y, increment, function) - derivative_y(x, y, increment, function)) / increment
-
-
-def derivative_yx(x, y, increment, function):
-    return (derivative_x(x, y + increment, increment, function) - derivative_x(x, y, increment, function)) / increment
-
-
-def derivative_yy(x, y, increment, function):
-    return (function(x, y + increment) - 2 * function(x, y) + function(x, y - increment)) / (increment ** 2)
-
-
-def gradient_method(init_x, init_y, epsilon, step, function):
-    path = [[init_x], [init_y]]
-    x_old_value = init_x
-    y_old_value = init_y
-    gradient_init = gradient(init_x, init_y, epsilon, function)
-    x_new_value = init_x - step * gradient_init[0][0]
-    y_new_value = init_y - step * gradient_init[1][0]
-    while abs(function(x_old_value, y_old_value) - function(x_new_value, y_new_value)) > epsilon:
-        x_old_value = x_new_value
-        y_old_value = y_new_value
-        gradient_new = gradient(x_new_value, y_new_value, epsilon, function)
-        x_new_value = x_new_value - step * gradient_new[0][0]
-        y_new_value = y_new_value - step * gradient_new[1][0]
-        path[0].append(x_new_value)
-        path[1].append(y_new_value)
-    return x_new_value, y_new_value, path
+@eel.expose
+def get_golden_ratio_method_result(function, a, b):
+    # answer = []
+    # for i in [0.1, 0.01, 0.001]:
+    #     x_p, R, d = logic.golden_ratio(a, b, i, logic.string_to_function(function))
+    #     answer.append([i, x_p, R, d, logic.count_of_calls])
+    #     x = np.arange(a, b, 0.01)
+    #     plt.clf()
+    #     plt.xlabel("x")
+    #     plt.ylabel("y")
+    #     y = [logic.string_to_function(function)(element) for element in x]
+    #     plt.plot(x, y)
+    #     plt.errorbar(x_p, R, xerr=d, marker='o', linestyle='none',
+    #     ecolor='k', elinewidth=0.8, capsize=4, capthick=1)
+    #     plt.grid()
+    #     name = "image/image" + str(i) + "_" + str(time.time()) + ".png"
+    #     plt.savefig("web/" + name, transparent=True)
+    #     answer[-1].append(name)
+    #     print(answer)
+    return 0
 
 
-def tuple2colvec(x: tuple) -> np.array:
-    return np.array(x)[np.newaxis, ...].T
+eel.init('web')
 
+eel.start('index.html')
 
-def colvec2tuple(x: np.array) -> tuple:
-    return tuple(x.T[0])
-
-
-def newton_method(init_x, epsilon, step, function):
-    path = [[init_x[0]], [init_x[1]]]
-
-    x = tuple2colvec(init_x)
-    for i in range(0, 100):
-        gradient_value = gradient(x[0][0], x[1][0], epsilon, function)
-        hessian = [[derivative_xx(x[0][0], x[1][0], epsilon, function),
-                    derivative_xy(x[0][0], x[1][0], epsilon, function)],
-                   [derivative_yx(x[0][0], x[1][0], epsilon, function),
-                    derivative_yy(x[0][0], x[1][0], epsilon, function)]]
-        x_old_value = x
-
-        x = x - step * (np.matmul(hessian, gradient_value))
-
-        if is_not_improved(x_old_value, x, epsilon):
-            break
-
-        path[0].append(x[0][0])
-        path[1].append(x[1][0])
-    return path[0][-1], path[1][-1], path
-
-def broyden_fletcher_goldfarb_shanno_method(init_x, epsilon, step, function):
-    path = [[init_x[0]], [init_x[1]]]
-
-    x = tuple2colvec(init_x)
-    x_old_value = None
-    for i in range(0, 100):
-        gradient_value = gradient(x[0][0], x[1][0], epsilon, function)
-
-        if x_old_value is None:
-            hessian = np.identity(x.shape[0])
-        else:
-            s = x - x_old_value
-            y = np.subtract(gradient_value, gradient_old_value)
-            hessian = (np.identity(x.shape[0]) - (s @ y.T) / (y.T @ s)) @ hessian @ (np.identity(x.shape[0]) - (y @ s.T) / (y.T @ s)) + ((s @ s.T) / (y.T @ s))
-
-        x_old_value = x
-
-        x = x - step * (np.matmul(hessian, gradient_value))
-
-        if is_not_improved(x_old_value, x, epsilon):
-            break
-
-        path[0].append(x[0][0])
-        path[1].append(x[1][0])
-        gradient_old_value = gradient_value
-    return path[0][-1], path[1][-1], path
-
-
-def is_not_improved(x_prev, x, epsilon):
-    return np.abs(x_prev - x).sum() < epsilon
-
-
-if __name__ == '__main__':
-    math_function = string_to_function(f)
-
-    x = np.linspace(-2, 4, 500)
-    y = np.linspace(-5, 1, 500)
-    X, Y = np.meshgrid(x, y)
-    F = string_to_function(f)(X, Y)
-
-    a_x, a_y, path = gradient_method(0, 0, 0.0001, 0.1, math_function)
-    print(a_x, a_y)
-    print(math_function(a_x, a_y))
-    print(path)
-
-    a_x, a_y, path = newton_method((0, 0), 0.0001, 0.01, math_function)
-    print(a_x, a_y)
-    print(math_function(a_x, a_y))
-    print(path)
-
-    a_x, a_y, path = broyden_fletcher_goldfarb_shanno_method((0, 0), 0.0001, 0.1, math_function)
-    print(a_x, a_y)
-    print(math_function(a_x, a_y))
-    print(path)
---
+++ MetodOptLab4/web/index.js
@@ -0,0 +1,362 @@
+let cursorPosition = 0;
+
+async function setCursorPosition(value) {
+	cursorPosition = value
+}
+
+async function getFibonacciMethodResult() {
+	document.getElementById("loader").style.display='flex';
+	let save_formula = document.getElementById('equation_showcase').cloneNode(true);
+	save_formula.id = '';
+	save_formula.className = '';
+    let equation = document.getElementById('equation').value;
+	let a = parseInt(document.getElementById('a_value').value);
+	let b = parseInt(document.getElementById('b_value').value);
+	let delta = parseInt(document.getElementById('delta_value').value);
+	if (equation === '' || isNaN(a) || isNaN(b)){
+		document.getElementById("loader").style.display='none';
+        return;
+    }
+	delta = isNaN(delta) ? 0.01 : delta;
+	console.log(equation, a, b, delta)
+
+    let output = document.getElementById("output");
+    let result = document.createElement('div');
+    result.className = 'result';
+	let calculations;
+	try {
+		calculations = await eel.get_fibonacci_method_result(equation, a, b, delta)();
+		document.getElementById("loader").style.display='none';
+	} catch (e) {
+		document.getElementById("loader").style.display='none';
+		result.innerHTML += "Ошибка в входных данных";
+		output.prepend(result);
+		return
+	}
+
+	result.innerHTML += "Исходная функция:"
+	result.appendChild(document.createElement('br'))
+	result.appendChild(document.createElement('br'))
+	let y_label = document.createElement('mi')
+	y_label.innerHTML = 'y'
+	let left_bracket = document.createElement('mo')
+	left_bracket.innerHTML = '('
+	let y_inner_label = document.createElement('mi')
+	y_inner_label.innerHTML = 'x'
+	let right_bracket = document.createElement('mo')
+	right_bracket.innerHTML = ')'
+	let equate = document.createElement('mo')
+	equate.innerHTML = '='
+	save_formula.prepend(equate)
+	save_formula.prepend(right_bracket)
+	save_formula.prepend(y_inner_label)
+	save_formula.prepend(left_bracket)
+	save_formula.prepend(y_label)
+	result.appendChild(save_formula)
+	result.appendChild(document.createElement('br'))
+	result.appendChild(document.createElement('br'))
+	calculations.map(answer => {
+		result.innerHTML += "Точность: "
+		result.innerHTML += answer[0]
+		result.appendChild(document.createElement('br'))
+		result.innerHTML += "X = "
+		result.innerHTML += answer[1].toFixed(6)
+		result.innerHTML += " ± "
+		result.innerHTML += answer[3].toFixed(6)
+		result.appendChild(document.createElement('br'))
+		result.innerHTML += "Количество итераций:"
+		result.innerHTML += answer[4]
+		result.appendChild(document.createElement('br'))
+		let img = new Image();
+		img.onload = function(){
+		  // execute drawImage statements here
+		};
+		img.src = answer[5];
+		result.appendChild(img);
+		result.appendChild(document.createElement('br'))
+	});
+
+	output.prepend(result);
+}
+
+async function getGoldenRatioMethodResult() {
+	document.getElementById("loader").style.display='flex';
+	let save_formula = document.getElementById('equation_showcase').cloneNode(true);
+	save_formula.id = '';
+	save_formula.className = '';
+    let equation = document.getElementById('equation').value;
+	let a = parseInt(document.getElementById('a_value').value);
+	let b = parseInt(document.getElementById('b_value').value);
+	if (equation === '' || isNaN(a) || isNaN(b)){
+		document.getElementById("loader").style.display='none';
+        return;
+    }
+
+    let output = document.getElementById("output");
+    let result = document.createElement('div');
+    result.className = 'result';
+	let calculations;
+	try {
+		calculations = await eel.get_golden_ratio_method_result(equation, a, b)();
+		document.getElementById("loader").style.display='none';
+	} catch (e) {
+		document.getElementById("loader").style.display='none';
+		result.innerHTML += "Ошибка в входных данных";
+		output.prepend(result);
+		return
+	}
+	result.innerHTML += "Исходная функция:"
+	result.appendChild(document.createElement('br'))
+	result.appendChild(document.createElement('br'))
+	let y_label = document.createElement('mi')
+	y_label.innerHTML = 'y'
+	let left_bracket = document.createElement('mo')
+	left_bracket.innerHTML = '('
+	let y_inner_label = document.createElement('mi')
+	y_inner_label.innerHTML = 'x'
+	let right_bracket = document.createElement('mo')
+	right_bracket.innerHTML = ')'
+	let equate = document.createElement('mo')
+	equate.innerHTML = '='
+	save_formula.prepend(equate)
+	save_formula.prepend(right_bracket)
+	save_formula.prepend(y_inner_label)
+	save_formula.prepend(left_bracket)
+	save_formula.prepend(y_label)
+	result.appendChild(save_formula)
+	result.appendChild(document.createElement('br'))
+	result.appendChild(document.createElement('br'))
+	calculations.map(answer => {
+		result.innerHTML += "Точность: "
+		result.innerHTML += answer[0]
+		result.appendChild(document.createElement('br'))
+		result.innerHTML += "X = "
+		result.innerHTML += answer[1].toFixed(6)
+		result.innerHTML += " ± "
+		result.innerHTML += answer[3].toFixed(6)
+		result.appendChild(document.createElement('br'))
+		result.innerHTML += "Количество итераций:"
+		result.innerHTML += answer[4]
+		result.appendChild(document.createElement('br'))
+		let img = new Image();
+		img.onload = function(){
+		  // execute drawImage statements here
+		};
+		img.src = answer[5];
+		result.appendChild(img);
+		result.appendChild(document.createElement('br'))
+	});
+
+	output.prepend(result);
+}
+
+async function buttonClick(data, move) {
+	let text = document.getElementById('equation').value;
+	document.getElementById('equation').value =
+		[text.slice(0, cursorPosition), data, text.slice(cursorPosition),].join('');
+	document.getElementById('equation').focus();
+	cursorPosition += move;
+	document.getElementById('equation').selectionStart = cursorPosition;
+    document.getElementById('equation').selectionEnd = cursorPosition;
+	parse(document.getElementById('equation').value);
+}
+
+async function clearInput() {
+	document.getElementById('equation').value = ''
+	document.getElementById('equation_showcase').innerHTML = ''
+}
+
+async function toMathMl(expression) {
+	let str = ``;
+	if (expression.Binary) {
+		if (expression.Binary.operator === '/') {
+			str += `<mfrac>`
+			if (expression.Binary.left.Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.Binary.left.Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.Binary.left).then(e => str = str +  e)
+			if (expression.Binary.right.Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.Binary.right.Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.Binary.right).then(e => str = str +  e)
+			str += `</mfrac>`
+		} else {
+			str += `<mrow>`
+			if (expression.Binary.left.Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.Binary.left.Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.Binary.left).then(e => str = str +  e)
+			str += `<mo>`
+			str = str + expression.Binary.operator
+			str += `</mo>`
+			if (expression.Binary.right.Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.Binary.right.Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.Binary.right).then(e => str = str +  e)
+			str += `</mrow>`
+		}
+	} else if (expression.Unary) {
+		str += `<mo>`
+		str = str + expression.Unary.operator
+		str += `</mo>`
+		if (expression.Unary.expression.Expression) {
+			str += `<mrow>`
+			str += `<mo>(</mo>`
+			await toMathMl(expression.Unary.expression.Expression).then(e => str = str +  e)
+			str += `<mo>)</mo>`
+			str += `</mrow>`
+		} else
+			await toMathMl(expression.Unary.expression).then(e => str = str +  e)
+	} else if (expression.FunctionCall) {
+		if (expression.FunctionCall.name === `factorial`) {
+			if (expression.FunctionCall.args[0].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[0].Expression).then(e => str = str + e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[0]).then(e => str = str +  e)
+			str += `<mo>`
+			str = str + '!'
+			str += `</mo>`
+		} else if (expression.FunctionCall.name === `sqrt`) {
+			if (expression.FunctionCall.args.length === 1 ||
+			   (expression.FunctionCall.args[expression.FunctionCall.args.length - 1].Number === '2')) {
+				str += `<msqrt>`
+				str += `<mrow>`
+				if (expression.FunctionCall.args[0].Expression) {
+					str += `<mrow>`
+					str += `<mo>(</mo>`
+					await toMathMl(expression.FunctionCall.args[0].Expression).then(e => str += e)
+					str += `<mo>)</mo>`
+					str += `</mrow>`
+				} else
+					await toMathMl(expression.FunctionCall.args[0]).then(e => str += e)
+				str += `</mrow>`
+				str += `</msqrt>`
+			} else {
+				str += `<mrow class="absolute"><msub class="down"><mi></mi>`
+				if (expression.FunctionCall.args[expression.FunctionCall.args.length - 1].Expression) {
+					str += `<mrow>`
+					str += `<mo>(</mo>`
+					await toMathMl(expression.FunctionCall.args[expression.FunctionCall.args.length - 1].Expression).then(e => str += e)
+					str += `<mo>)</mo>`
+					str += `</mrow>`
+				} else
+					await toMathMl(expression.FunctionCall.args[expression.FunctionCall.args.length - 1]).then(e => str += e)
+				str += `</msub>`
+				str += `<msqrt><mrow>`
+				if (expression.FunctionCall.args[0].Expression) {
+						str += `<mrow>`
+						str += `<mo>(</mo>`
+						await toMathMl(expression.FunctionCall.args[0].Expression).then(e => str += e)
+						str += `<mo>)</mo>`
+						str += `</mrow>`
+					} else
+						await toMathMl(expression.FunctionCall.args[0]).then(e => str += e)
+
+				str += `</mrow>`
+				str += `</msqrt></mrow>`
+			}
+		}  else if (expression.FunctionCall.name === `pow`) {
+			str += `<msup>`
+			if (expression.FunctionCall.args[0].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[0].Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[0]).then(e => str = str +  e)
+			if (expression.FunctionCall.args[1].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[1].Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[1]).then(e => str = str +  e)
+			str += `</msub>`
+		} else if (expression.FunctionCall.name === `log`) {
+			str += `<mrow><msub>`
+			str += `<mi>${expression.FunctionCall.name}</mi>`
+			if (expression.FunctionCall.args[1].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[1].Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[1]).then(e => str = str +  e)
+			str += `</msub>`
+			if (expression.FunctionCall.args[0].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[0].Expression).then(e => str = str +  e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[0]).then(e => str = str +  e)
+			str += `</mrow>`
+		} else {
+			str += `<mrow>`
+			str += "<mo>" + expression.FunctionCall.name + "</mo>"
+			for (let arg_index = 0; arg_index < expression.FunctionCall.args.length - 1; arg_index++) {
+				if (expression.FunctionCall.args[arg_index].Expression) {
+					str += `<mrow>`
+					str += `<mo>(</mo>`
+					await toMathMl(expression.FunctionCall.args[arg_index].Expression).then(e => str += e)
+					str += `<mo>)</mo>`
+					str += `</mrow>`
+				} else
+					await toMathMl(expression.FunctionCall.args[arg_index]).then(e => str += e)
+				if (arg_index < expression.FunctionCall.args.length - 2) {
+					str += `<mo>`
+					str += ','
+					str += `</mo>`
+				}
+			}
+			if (expression.FunctionCall.args[expression.FunctionCall.args.length - 1].Expression) {
+				str += `<mrow>`
+				str += `<mo>(</mo>`
+				await toMathMl(expression.FunctionCall.args[expression.FunctionCall.args.length - 1].Expression).then(e => str += e)
+				str += `<mo>)</mo>`
+				str += `</mrow>`
+			} else
+				await toMathMl(expression.FunctionCall.args[expression.FunctionCall.args.length - 1]).then(e => str += e)
+			str += `</mrow>`
+		}
+	} else if (expression.Identifier) {
+		str += `<mi>`
+		str += expression.Identifier === `pi` ? 'π' : expression.Identifier
+		str += `</mi>`
+	} else if (expression.Number) {
+		str += `<mn>`
+		str += expression.Number
+		str += `</mn>`
+	}
+	return str
+}
+
+const convertStringToMath = async htmlString => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(htmlString, 'text/html');
+    return html.body.innerHTML;
+}
