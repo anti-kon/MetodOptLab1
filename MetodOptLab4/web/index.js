bb776c999b912ccbfff9775c62090724149fe771
Anton Konopelkin <anti.kon.40@yandex.ru>
2024-04-10 13:22:43 +0300

    Metod 4

--- MetodOptLab4/web/index.js
+++ MetodOptLab4/web/index.js
@@ -74,13 +74,13 @@ async function getGradientMethodResult() {
 		result.innerHTML += answer[0]
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Ответ: ("
-		result.innerHTML += answer[1].toFixed(answer[0])
+		result.innerHTML += answer[1] % 1 === 0 ? answer[1] + ".0" : Math.trunc(answer[1]) + "." + String((answer[1] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ", "
-		result.innerHTML += answer[2].toFixed(answer[0])
+		result.innerHTML += answer[2] % 1 === 0 ? answer[2] + ".0" : Math.trunc(answer[2]) + "." + String((answer[2] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ")"
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Значение: "
-		result.innerHTML += answer[3].toFixed(answer[0])
+		result.innerHTML += answer[3] % 1 === 0 ? answer[3] + ".0" : Math.trunc(answer[3]) + "." + String((answer[3] % 1).toPrecision(1)).split('.')[1]
 		result.appendChild(document.createElement('br'))
 		let img1 = new Image();
 		img1.onload = function(){
@@ -177,13 +177,13 @@ async function getNewtonMethodResult() {
 		result.innerHTML += answer[0]
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Ответ: ("
-		result.innerHTML += answer[1].toFixed(answer[0])
+		result.innerHTML += answer[1] % 1 === 0 ? answer[1] + ".0" : Math.trunc(answer[1]) + "." + String((answer[1] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ", "
-		result.innerHTML += answer[2].toFixed(answer[0])
+		result.innerHTML += answer[2] % 1 === 0 ? answer[2] + ".0" : Math.trunc(answer[2]) + "." + String((answer[2] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ")"
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Значение: "
-		result.innerHTML += answer[3].toFixed(answer[0])
+		result.innerHTML += answer[3] % 1 === 0 ? answer[3] + ".0" : Math.trunc(answer[3]) + "." + String((answer[3] % 1).toPrecision(1)).split('.')[1]
 		result.appendChild(document.createElement('br'))
 		let img1 = new Image();
 		img1.onload = function(){
@@ -276,13 +276,13 @@ async function getBFGSResult() {
 		result.innerHTML += answer[0]
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Ответ: ("
-		result.innerHTML += answer[1].toFixed(answer[0])
+		result.innerHTML += answer[1] % 1 === 0 ? answer[1] + ".0" : Math.trunc(answer[1]) + "." + String((answer[1] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ", "
-		result.innerHTML += answer[2].toFixed(answer[0])
+		result.innerHTML += answer[2] % 1 === 0 ? answer[2] + ".0" : Math.trunc(answer[2]) + "." + String((answer[2] % 1).toPrecision(1)).split('.')[1]
 		result.innerHTML += ")"
 		result.appendChild(document.createElement('br'))
 		result.innerHTML += "Значение: "
-		result.innerHTML += answer[3].toFixed(answer[0])
+		result.innerHTML += answer[3] % 1 === 0 ? answer[3] + ".0" : Math.trunc(answer[3]) + "." + String((answer[3] % 1).toPrecision(1)).split('.')[1]
 		result.appendChild(document.createElement('br'))
 		let img1 = new Image();
 		img1.onload = function(){
